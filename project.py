a59554cf5a6703d82d466ff3a1e22fdb12d779a3
Carlos Caro <carlosjesuscaro@icloud.com>
2018-02-04 15:04:10 -0500

    Delete Menu Item

--- project.py
+++ project.py
@@ -59,7 +59,14 @@ def editMenuItem(restaurant_id, menu_id):
 # Task 3: Create a route for deleteMenuItem function here
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/', methods = ['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
-    return "page to delete a menu item. Task 3 complete!"
+    deletedMenuItem = session.query(MenuItem).filter_by(id = menu_id).one()
+    if request.method == 'POST':
+    	session.delete(deletedMenuItem)
+    	session.commit()
+    	return redirect(url_for('RestaurantMenu', restaurant_id = restaurant_id))
+    else:
+    	return render_template('deletemenuitem.html', restaurant_id = restaurant_id,
+    		menu_id = menu_id, item = deletedMenuItem)
 
 if __name__ == '__main__':
 	app.debug = True
